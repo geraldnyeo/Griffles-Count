53c424df48db37bf481dc1fefeb2d661db7bf7cb
Gerald <geraldnyeo@gmail.com>
2021-12-05 09:28:51 +0800

    Bug fixes and image added
    Changes
     - Griffles image is now in an appropriate location (sry, I can't do the pop out from side thing, esp since I can't find a good photo)
     - Speech bubble burnt into image to make positioning easier and so it won't rescale weirdly for different display sizes
     - Bug fixes in the loading and setting of data
    Notes
     - Currently users can edit the form before the data loads, but once the data loads it will override their changes. Can either:
     a. Keep it this way and hope data loads really fast.
     b. Prevent users from editing before the data loads.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ I'm leaving everything in a single js file for now so I don't have to deal with 
 Which means it may be a little messy sorry...
 */
 
-// Box component being unnecessary layers are my specialty
+// Box component: unnecessary layers are my specialty
 // (it's just the part with the header and the form)
 class Box extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
+            first: true,
             guess: "",
             email: "",
             status: "",
@@ -18,6 +20,11 @@ class Box extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+    }
+
+    async componentDidMount() {
+        const load = await this.getStatus();
+        this.setState(load);
     }
 
     handleChange(event) {
@@ -39,70 +46,6 @@ class Box extends React.Component {
     }
 
     // fake API to simulate lag times in data fetching
-    async setStatus(guess, email) {
-        return new Promise(res => {setTimeout(() => {
-                res(1);
-            }, 2000)
-        });
-    }
-
-    render() {
-        const headerText = this.props.first ? "COUNT Griffles" : "Think your answer was wrong?";
-        const headerClass = this.props.first ? "text-3xl" : "text-2xl";
-
-        return (
-            <div id="box" className="flex flex-col items-start static rounded-md border-2 border-black shadow-lg p-5 space-y-3 sm:w-1/2 md:w-1/3 xl:w-1/4">
-                <h1 id="header" className={headerClass}>{headerText}</h1>
-                <p id="info">Griffles is hiding in a few locations around the website. Guess how many times Griffles appeared!</p> {/*Filler infotext*/}
-
-                {/*Form, but no form tag because don't want page to keep reloading - forms do their own thing in HTML*/}
-                <div className="flex flex-row space-x-2">
-                    <label htmlFor="guess">Your Guess:</label>
-                    <input id="guess" className="rounded-lg border border-black px-1" name="guess" placeholder="number of Griffles" value={this.state.guess} onChange={this.handleChange}></input>
-                </div>
-                <div className="flex flex-row space-x-2">
-                    <label htmlFor="email">Email:</label>
-                    <input id="email" className="rounded-lg border border-black px-1" name="email" placeholder="example@xyz.com" value={this.state.email} onChange={this.handleChange}></input>
-                </div>
-                <button id="submit" className="rounded-full bg-green-500 px-2 py-1 text-white" onClick={this.handleClick}>Submit Guess!</button>
-                
-                <p id="success" className="bg-green-200 p-2">{this.state.status}</p>
-            </div>
-        )
-    }
-}
-
-const Griffles = props => {
-    return(
-        <div id="Griffles" className="absolute z-1">
-            <img id="Griffles" src="../assets/Gariffles.png" alt="Griffles says:"></img>
-            {/* Change Gariffles back Griffles when done. It's just difficult to do CSS when the giant griffles img is blocking everything lol */}
-            <p id="speech">
-                Include me!
-            </p>
-        </div>
-    )
-}
-
-// App component contains all the rest of the stuff
-// and handles the backend and everything because its the top level component (idk if that's a term)
-// Using a stateful component because wHy NOt (too lazy to optimise code rn lol)
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            first: true,
-            guess: "",
-            email: "",
-        }
-    }
-
-    async componentDidMount() {
-        this.setState(await this.getStatus);
-    }
-
-    // fake API to simulate lag times in data fetching
     async getStatus() {
         return new Promise(res => {setTimeout(() => {
                 res({
@@ -114,11 +57,66 @@ class App extends React.Component {
         });
     }
 
+    // fake API to simulate lag times in data fetching
+    async setStatus(guess, email) {
+        return new Promise(res => {setTimeout(() => {
+                res(1);
+            }, 2000)
+        });
+    }
+
+    render() {
+        const headerText = this.state.first ? "COUNT Griffles" : "Think your answer was wrong?";
+        const headerClass = this.state.first ? "text-3xl" : "text-2xl";
+        const statusElement = (this.state.status != "") ? <p id="success" className="bg-green-200 p-2">{this.state.status}</p> : null;
+
+        return (
+            <div id="box" className="container flex flex-col items-start sm:w-1/2 md:w-1/3 xl:w-1/4">
+                <Griffles first={this.state.first} />
+
+                <div className="static rounded-md border-2 border-black shadow-lg p-5 space-y-3">
+                    <h1 id="header" className={headerClass}>{headerText}</h1>
+                    <p id="info">Griffles is hiding in a few locations around the website. Guess how many times Griffles appeared!</p> {/*Filler infotext*/}
+
+                    {/*Form, but no form tag because don't want page to keep reloading - forms do their own thing in HTML*/}
+                    <div id="main-content" className="flex flex-row space-x-2">
+                        <label htmlFor="guess">Your Guess:</label>
+                        <input id="guess" className="rounded-lg border border-black px-1" name="guess" placeholder="number of Griffles" value={this.state.guess} onChange={this.handleChange}></input>
+                    </div>
+                    <div className="flex flex-row space-x-2">
+                        <label htmlFor="email">Email:</label>
+                        <input id="email" className="rounded-lg border border-black px-1" name="email" placeholder="example@xyz.com" value={this.state.email} onChange={this.handleChange}></input>
+                    </div>
+                    <button id="submit" className="rounded-full bg-green-500 px-2 py-1 text-white" onClick={this.handleClick}>Submit Guess!</button>
+                    
+                    {statusElement}
+                </div>
+            </div>
+        )
+    }
+}
+
+const Griffles = props => {
+    return(
+        <div id="Griffles" className="relative z-1">
+            <img id="Griffles" className="w-2/3" src="../assets/Griffles.png" alt="Griffles says:"></img>
+        </div>
+    )
+}
+
+// App component contains all the rest of the stuff
+// and handles the backend and everything because its the top level component (idk if that's a term)
+// after changing up the logic a bit, the BOX component now handles the calls and stuff (as you can see, unnecessary layers but yay)
+// which makes this pretty useless but I always like having 1 top level component to contain everything else, in case need to add other components or whatever
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div id="app" className="relative">
-                <Griffles first={this.state.first} firstGuess={this.state.guess} firstEmail={this.state.email} />
-                <Box first={this.state.first} />
+                <Box />
             </div>
         )
     }
